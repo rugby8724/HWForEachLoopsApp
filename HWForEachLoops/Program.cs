05b2813dad0c49129d37aace4b065732d67d5fbb
Jeremiah Wise <jeremiah.d.wise@gmail.com>
2023-07-27 14:26:38 -0500

    Add project files.

---
+++ HWForEachLoops/Program.cs
@@ -0,0 +1,26 @@
+﻿
+
+List<string> names = new List<string>();
+bool continueProgram = true;
+
+do
+{
+    Console.WriteLine("Hello, Please enter a name");
+    string enteredName = Console.ReadLine();
+    if (enteredName != null || enteredName != "")
+    {
+        names.Add(enteredName);
+    }
+    Console.WriteLine("If you're finished entering names press E, else press enter to continue");
+    string? response = Console.ReadLine();
+    if (response == "e")
+    {
+        continueProgram = false;
+    }
+
+} while (continueProgram);
+
+foreach (var name in names)
+{
+    Console.WriteLine($"Hello {name}");
+}
